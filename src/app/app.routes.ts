f6a35501cff13083b81fd6accf14aa29d822e8dc
angelporlan <angeelporlan4@gmail.com>
2025-04-27 13:40:27 +0200

    add routing and HTTP client configuration to app setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StoreComponent } from './components/store/store.component';
+import { MenuComponent } from './views/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'store', component: StoreComponent },
+  { path: '**', redirectTo: '' }
+];
